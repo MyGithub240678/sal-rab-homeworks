33b02498ee6d60c12cbb0ceb2db58a16122d835a
Neto test <lettersmail@yandex.ru>
2023-03-12 18:29:13 +0300

    task_3 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,16 +6,14 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     let shippingSum; // создайте переменную shippingSum
- 
-    if (productsSum == 0) {
+    
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-       
-   } else if (productsSum >= freeShippingMinSum) {
-       shippingSum = 0; 
-       } else {
-       shippingSum = shippingPrice;
-       
-   }
+      }
+      
+    else {
+        shippingSum = shippingPrice;
+    }
     // если productsSum равно 0, 
     // то shippingSum присвоить значение 0
 
